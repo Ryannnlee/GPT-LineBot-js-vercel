9850c5228aaf7a48de8cce2a03d2bc5b7926f25c
Ryannnlee <lee810317@hotmail.com.tw>
2022-12-18 18:18:47 +0800

    update webhook

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const app = express()
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
+app.post('/webhook', line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
