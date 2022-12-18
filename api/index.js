d345157b9cc3fc72b37c67b5f26a3fdc090079b2
Ryannnlee <lee810317@hotmail.com.tw>
2022-12-18 18:23:44 +0800

    add gpt

--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@ require('dotenv').config()
 
 const express = require('express')
 const line = require('@line/bot-sdk')
+const { Configuration, OpenAIApi } = require('openai')
 
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 }
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
 
 // create LINE SDK client
 const client = new line.Client(config)
@@ -27,14 +33,20 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 })
 
 // event handler
-function handleEvent(event) {
+async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null)
   }
 
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: event.message.text,
+    max_tokens: 200,
+  })
+
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text }
+  const echo = { type: 'text', text: completion.data.choices[0].text.trim() }
 
   // use reply API
   return client.replyMessage(event.replyToken, echo)
