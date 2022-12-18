acfa296a133a1a105550ec4103d3c31b0701610f
Ryannnlee <lee810317@hotmail.com.tw>
2022-12-18 18:11:31 +0800

    update

--- api/index.js
+++ api/index.js
@@ -1,42 +1,47 @@
-import express from 'express'
-import Assistant from '../assistant/index.js'
-import config from '../config/index.js'
-import { validateLineSignature } from '../middleware/index.js'
+require('dotenv').config()
 
-const assistant = new Assistant()
+const express = require('express')
+const line = require('@line/bot-sdk')
 
-const app = express()
-
-app.use(
-  express.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf.toString()
-    },
-  })
-)
-
-app.get('/', (req, res) => {
-  if (config.APP_URL) {
-    res.redirect(config.APP_URL)
-    return
-  }
-  res.sendStatus(200)
-})
-
-app.post('/webhook', validateLineSignature, async (req, res) => {
-  try {
-    await assistant.handleEvents(req.body.events)
-  } catch (err) {
-    console.error(err)
-    res.sendStatus(500)
-    return
-  }
-  if (config.APP_DEBUG) assistant.printPrompts()
-  res.sendStatus(200)
-})
-
-if (config.APP_PORT) {
-  app.listen(config.APP_PORT)
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 }
 
-export default app
+// create LINE SDK client
+const client = new line.Client(config)
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express()
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err)
+      res.status(500).end()
+    })
+})
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null)
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text }
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo)
+}
+
+// listen on port
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
